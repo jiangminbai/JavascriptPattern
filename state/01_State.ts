a7ade6f7aac8417e0d9a8d6d3e428d80cbbcf3fe
jan-wong <543050768@qq.com>
2019-08-10 19:52:10 +0800

    update state

--- state/01_State.ts
+++ state/01_State.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env ts-node
 /**
- * 状态模式
+ * 状态模式：允许对象在内部状态改变时改变它的行为，对象看起来好像修改它的类。
  * 场景：糖果机投放25美分，转动曲柄，糖果机就会派送糖果。包含4种状态和4种动作。
  *      4种状态：没有25美分、有25美分、糖果售罄、售出糖果；
  *      4种动作：投入25美分、退回25美分、转动曲柄、派送糖果。调用每个动作都会触发状态之间的转换。
- * 角色：
+ * 角色：糖果机类、状态接口和具体状态类
  */
 
 // 状态接口
@@ -28,11 +28,18 @@ class NoQuarterState implements State {
   }
 
   insertQuarter(){
+    console.log('you have insert quarter');
     this.gumballMachine.setState(this.gumballMachine.getHasQuarterState());
   }
-  ejectQaurter() {}
-  turnCrank() {}
-  dispense(){}
+  ejectQaurter() {
+    console.log('you does not insert quarter');
+  }
+  turnCrank() {
+    console.log('turncrank does not effect');
+  }
+  dispense(){
+    console.log('please insert quarter');
+  }
 }
 
 // 有25美分的状态类
@@ -43,10 +50,20 @@ class HasQuarterState implements State {
     this.gumballMachine = gumballMachine;
   }
 
-  insertQuarter() {};
-  ejectQaurter() {};
-  turnCrank() {};
-  dispense() {};
+  insertQuarter() {
+    console.log('you cannot insert another quarter');
+  };
+  ejectQaurter() {
+    console.log('quarter returned');
+    this.gumballMachine.setState(this.gumballMachine.getNoQuarterState());
+  };
+  turnCrank() {
+    console.log('turning...');
+    this.gumballMachine.setState(this.gumballMachine.getSoldState());
+  };
+  dispense() {
+    console.log('no gumbal dispense');
+  };
 }
 
 // 售出糖果的状态类
@@ -57,10 +74,24 @@ class SoldState implements State {
     this.gumballMachine = gumballMachine;
   }
 
-  insertQuarter() {};
-  ejectQaurter() {};
-  turnCrank() {};
-  dispense() {};
+  insertQuarter() {
+    console.log('please wait, we has given you a gumball');
+  };
+  ejectQaurter() {
+    console.log('sorry wo has turned the crank');
+  };
+  turnCrank() {
+    console.log('turning twice does not giving you another gumball');
+  };
+  dispense() {
+    this.gumballMachine.releaseBall();
+    if (this.gumballMachine.getCount() > 0) {
+      this.gumballMachine.setState(this.gumballMachine.getNoQuarterState());
+    } else {
+      console.log('out of the gumball');
+      this.gumballMachine.setState(this.gumballMachine.getSoldOutState());
+    }
+  };
 }
 
 // 糖果售罄的状态类
@@ -71,10 +102,19 @@ class SoldOutState implements State {
     this.gumballMachine = gumballMachine;
   }
 
-  insertQuarter() {};
-  ejectQaurter() {};
-  turnCrank() {};
-  dispense() {};
+  insertQuarter() {
+    console.log('sorry, gumball sold out');
+    this.ejectQaurter();
+  };
+  ejectQaurter() {
+    console.log('your quarter ejected');
+  };
+  turnCrank() {
+    console.log('sorry, gumball sold out');
+  };
+  dispense() {
+    console.log('sorry, gumball sold out');
+  };
 }
 
 // 糖果机类
@@ -118,13 +158,35 @@ class GumballMachine {
   //   this.state.dispense();
   // }
 
+  releaseBall() {
+    if (this.count) {
+      this.count--;
+    }
+  }
+
   // 
   setState(state: State): void {
     this.state = state;
   }
 
   // 获取各种状态
+  getNoQuarterState() {
+    return this.NO_QUARTER;
+  }
+
   getHasQuarterState() {
     return this.HAS_QUARTER;
+  }
+
+  getSoldState() {
+    return this.SOLD;
+  }
+
+  getSoldOutState() {
+    return this.SOLD_OUT;
+  }
+
+  getCount() {
+    return this.count;
   }
 }
