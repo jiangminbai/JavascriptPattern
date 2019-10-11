5337535a408b9adfdc8f76ee0a78b490f82853bc
jan-wong <543050768@qq.com>
2019-10-11 21:45:38 +0800

    complete flyweight

---
+++ pattern/flyweight/01_Flyweight.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env ts-node
+/**
+ * 享元模式：运用共享技术有效地支持大量细粒度的对象
+ */
+
+// 树管理器类
+class TreeManager {
+  treeStatusArray = []; // 存储所有树的状态
+  tree: Tree; // 可以被重复利用的树对象
+
+  displayTrees() {
+    this.treeStatusArray = [{x: 0, y: 0, age: 1}, {x: 1, y: 1, age: 2}, {x: 3, y: 4, age: 5}];
+    for (let i = 0; i < this.treeStatusArray.length; i++) {
+      const treeStatus = this.treeStatusArray[i];
+      if (!this.tree) this.tree = new Tree();
+      this.tree.display(treeStatus.x, treeStatus.y, treeStatus.age);
+    }
+  }
+}
+
+// 树类
+class Tree {
+  display(x: number, y: number, age: number) {
+    console.log(`xcoord is ${x}, ycoord is ${y}, age is ${age}`);
+  }
+}
+
+class Client {
+  constructor() {
+    const treeManager = new TreeManager();
+    treeManager.displayTrees();
+  }
+}
+
+new Client();
